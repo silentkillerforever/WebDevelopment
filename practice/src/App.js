57f8f7ea8ca3059ee14c63fac44b47d4ba828fd1
Ajay <ajaykumarreddy@syscloudsoftware.com>
2022-09-23 10:45:30 +0530

    started working on new component - CardPractice

--- practice/src/App.js
+++ practice/src/App.js
@@ -1,17 +1,21 @@
 import './App.css';
-import Card from './components/Card'
+// import Card from './components/Card'
+import CardPractice from './components/CardPractice';
 // import Greet from './components/Greet';
-import king from './components/table.jpg';
+// import king from './components/table.jpg';
 // import { Home } from './components/Home';
 
 // import Chores from './components/Chores';
 function App() {
   return (
     <div className="App">
-      <Card
+      {/* <Card
       title='Card'
       imageURL={king}
-      body='Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industrys standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book'/>
+      body='Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industrys standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book'/> */}
+      <h1 className='hh1'>Ajay</h1>
+      <h1 className='hh2'>Ajay</h1>
+      <CardPractice/>
     </div>
   );
 }
---
+++ practice/src/components/CardPractice.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+import './CardPractice.css'
+
+function Portf({name,imageURL,dob,contactNo,company}){
+    return(
+        <div>Ajay</div>
+    )
+}
+
+export default Portf
